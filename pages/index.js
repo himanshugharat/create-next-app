9f46171a9fd8019d2f971e47a7f0923b5da05e88
himanshu <himanshugharat44@gmail.com>
2021-04-10 20:31:41 +0530

    added: the nabbar and footer to all page

--- pages/index.js
+++ pages/index.js
@@ -7,12 +7,12 @@ import Link from 'next/link'
 export default function Home() {
   return (
    <div>
-     <Navbar/>
+     
      <h1>homepage</h1>
      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt possimus odio soluta corrupti harum eligendi magnam vitae cupiditate quia dolorum nam nemo quod, adipisci voluptatum ipsam quaerat asperiores dolore ex.</p>
      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt possimus odio soluta corrupti harum eligendi magnam vitae cupiditate quia dolorum nam nemo quod, adipisci voluptatum ipsam quaerat asperiores dolore ex.</p>
    <Link href="/page"><a>list</a></Link>
-   <Footer/>
+  
    </div>
   )
 }
