bff08a34028fa1d5c1c1bb9b38784db29aca6daa
himanshu <himanshugharat44@gmail.com>
2021-04-10 22:09:06 +0530

    added: 404 page redirect to homepage

--- pages/404.js
+++ pages/404.js
@@ -1,9 +1,22 @@
 import Link from "next/link";
-const notFound=()=>{
-   return( <div className="not-found">
-<h1>oops...</h1>
-<h2>that page cannot be found</h2>
-<p>go back to the <Link href="/">Homepage</Link></p>
-    </div>)
-}
-export default notFound
+import { useEffect } from "react";
+import { useRouter} from "next/router";
+const notFound = () => {
+    const router=useRouter();
+  useEffect(() => {
+    setTimeout(() => {
+        // router.go(-1)
+        router.push("/")
+    }, 3000);
+  }, []);
+  return (
+    <div className="not-found">
+      <h1>oops...</h1>
+      <h2>that page cannot be found</h2>
+      <p>
+        go back to the <Link href="/">Homepage</Link>
+      </p>
+    </div>
+  );
+};
+export default notFound;
