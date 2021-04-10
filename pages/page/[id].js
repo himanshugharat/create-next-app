758ede69a44cceb334815e467301d34c6b3fb5bb
himanshu <himanshugharat44@gmail.com>
2021-04-10 23:22:32 +0530

    added: the project is ready

--- pages/page/[id].js
+++ pages/page/[id].js
@@ -1,8 +1,32 @@
-const detail=()=>{
-    return(
-        <div>
-            <h1>detaill page</h1>
-              </div>
-    )
-}
+export const getStaticPaths = async () => {
+  const re = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await re.json();
+  const paths = data.map((re) => {
+    return {
+      params: { id: re.id.toString() },
+    };
+  });
+  return {
+    paths,
+    fallback: false,
+  };
+};
+export const getStaticProps = async (context) => {
+  const id = context.params.id;
+  const res = await fetch("https://jsonplaceholder.typicode.com/users/" + id);
+  const data =   await res.json()
+  return {
+    props: { ninja: data },
+  };
+};
+
+const detail = ({ ninja }) => {
+  return (
+    <div>
+      <h1>{ninja.name}</h1>
+      <p>{ninja.email}</p>
+      <p>{ninja.address.city}</p>
+    </div>
+  );
+};
 export default detail;
