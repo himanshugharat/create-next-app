2a4454079ee919c140177390bd9a422a36ab9127
himanshu <himanshugharat44@gmail.com>
2021-04-14 14:52:42 +0530

    final done

--- component/Footer.js
+++ component/Footer.js
@@ -1,8 +1,14 @@
-const Footer=()=>{
-return(
+const Footer = () => {
+  return (
     <footer>
-        Copyright 2021
+      <script
+        async
+        id="frase-script"
+        data-hash="1b2c5fe323f54172b0b0e14cc1fba526"
+        src="https://app.frase.io/js/libraries/bot.js"
+      ></script>
+      Copyright 2021
     </footer>
-)
-}
-export default Footer
+  );
+};
+export default Footer;
